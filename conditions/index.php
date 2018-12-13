6f51033ee6b480a44039526fb0e79c9652509014
merliti.tillart <merliti.tillart@khk.ee>
2018-12-13 16:12:12 +0200

    Tingimused - if else ja $varv ja $arv

--- conditions/index.php
+++ conditions/index.php
@@ -2,16 +2,27 @@
 // tingimuslaused
 // if(tingimus) {
 // tegevused, mis toimuvad kui tingimus kehtib
-//} else {
-// tegevused, mis toimuvad kui tingimus ei kehti
-// }
+//} else if{
+// tegevused, mis toimuvad kui tingimus 1 ei kehti ja kehtib tingimus 2
+// }else
 
 echo '<h4>Tingimuslaused</h4>';
 
 $arv = rand(0, 100); //juhuslik täisarv
+$varv = '';
 
-if($arv % 2 == 0) {
-    echo '<p style="color: green">'.$arv.'</p>';
-}else{
-    echo '<p style="color: red">'.$arv.'</p>';
+if($arv > 0 and $arv < 25) {
+    // roheline
+    $varv = 'green';
+}else if ($arv >25 and $arv < 50) {
+    // punane
+    $varv = 'red';
+}else if ($arv >50 and $arv < 75) {
+    // sinine
+    $varv = 'blue';
+}else if ($arv >75 and $arv < 101) {
+    // punane
+    $varv = 'orange';
 }
+    echo '<p style="color: '.$varv.'">'.$arv.'</p>';
+
