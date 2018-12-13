20cf8546f55c2b893850ce9b5391c03c1a028000
merliti.tillart <merliti.tillart@khk.ee>
2018-12-13 15:04:39 +0200

    Väljastus -print ja echo

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+//teksti väljastus print käsuga
+print 'see on esimene lause php-keeles'.'<br>';
+print 'see on teine lause php-keeles';
+//teksti väljastus echo käsuga
+echo 'see tekts on echo abil väljatatud'.'<br>'
+
+
+?>
