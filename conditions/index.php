5cde1638d9890a5d70a62f89fbaddc57b4088902
merliti.tillart <merliti.tillart@khk.ee>
2018-12-13 16:19:00 +0200

    Tingimused - switch'i kaustamine- valgusfoor

--- conditions/index.php
+++ conditions/index.php
@@ -8,21 +8,20 @@
 
 echo '<h4>Tingimuslaused</h4>';
 
-$arv = rand(0, 100); //juhuslik täisarv
-$varv = '';
+$foor = 'kollane';
 
-if($arv > 0 and $arv < 25) {
-    // roheline
-    $varv = 'green';
-}else if ($arv >25 and $arv < 50) {
-    // punane
-    $varv = 'red';
-}else if ($arv >50 and $arv < 75) {
-    // sinine
-    $varv = 'blue';
-}else if ($arv >75 and $arv < 101) {
-    // punane
-    $varv = 'orange';
+switch ($foor) {
+    case 'punane':
+        echo '<p style="color: red">Punane!</p>';
+        break;
+    case 'kollane':
+        echo '<p style="color: yellow">Kollane!</p>';
+        break;
+    case 'roheline':
+        echo '<p style="color: yellow">Roheline!</p>';
+        break;
+    default:
+        echo '<p>mingi teine</p>';
+        break;
 }
-    echo '<p style="color: '.$varv.'">'.$arv.'</p>';
 
