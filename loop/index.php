41fd3fd1bca2a79542d5878aa090164432e4286d
merliti.tillart <merliti.tillart@khk.ee>
2018-12-14 10:24:56 +0200

    Tingimused - värve muutev tabel 5*6 ruutu

--- loop/index.php
+++ loop/index.php
@@ -1,10 +1,10 @@
 <?php
-/
+
 /* for tsükkel
  for(@jm = alg; $jm < lopp;$jm++) {
   tegevused, mis sooritatakse nii kaua kui tsükli $jm
 väärtus on väiksem kui lõppväärtus*/
-
+/*
 echo '<h4> Tsüklid -for</h4>';
 for($arv = 1; $arv <= 10; $arv++){
     if($arv % 2 == 0){
@@ -13,15 +13,22 @@ for($arv = 1; $arv <= 10; $arv++){
         $varv = 'red';
     }
     echo '<p style="color: '.$arv.'">'.$arv.'</p>';
-}
+}*/
 
 $varv = '#';
 echo '<table>';
 for($rida = 1; $rida <= 6; $rida++) {
     $varv = $varv.rand(0,9).rand(0,9).rand(0,9);
-    echo '<tr>'
-        echo '<td style="background: '.$varv.'">';
-        echo '&nbsp;&nbsp;&nbsp;&nbsp;';
+    echo '<tr>';
+    for($veerg = 1; $veerg <= 5; $veerg++) {
+        $varv = '#';
+        for($kord = 1; $kord <= 6; $kord++){
+            $varv = $varv.dechex(rand(0,15));
+        }
+        echo '<td style="background: ' . $varv . '">';
+        echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
         echo '</td>';
+    }
     echo '</tr>';
 }
+
