dd88a6b59a2e737120731fdee16535abe111cf20
merliti.tillart <merliti.tillart@khk.ee>
2018-12-18 12:48:46 +0200

    Array- väljastamine 'foreach''iga Massiiv masiivis

--- arrays/index.php
+++ arrays/index.php
@@ -1,28 +1,26 @@
 <?php
-$kasutajad = array();//tühi massiiv
-//andmete lisamine
-$kasutajad[]= 'Alice';
-$kasutajad[]= 'Aice';
-array_push($kasutajad,'Aucy');
-
-$lisaKasutajad = array('Mup', 'Geene'); //andmetega massiiv
-
-$kasutajadKokku = count($kasutajad);
-for($i=0;$i< $kasutajadKokku; $i++){
-    echo $kasutajad[$i].'<br>';
-}
-
-array_shift($kasutajad);
-for($i=0; $i < $kasutajadKokku; $i++){
-    echo $kasutajad[$i].'<br>';
-}
-$uuedKasutajad = array_merge($kasutajad, $lisaKasutajad);
-for($i=0; $i< $kasutajadKokku; $i++){
-    echo $uuedKasutajad[$i].'<br>';
-}
-echo '<hr>';
-
-foreach ($uuedKasutajad as $kasutaja){
-    echo $kasutaja.'<br>';
+$kasutajad = array(
+    array(
+        'cat',
+        'Marru',
+        'Barbar',
+        'male'
+    ),
+    array('fish',
+        'Kubo',
+        'Flop',
+        'female'
+        ),
+);
+for($i = 0; $i < count($kasutajad); $i++) {
+    foreach ($kasutajad[$i] as $info) {
+        if ($kasutajad[$i][3] == 'female') {
+            echo '<div style="color: crimson">';
+        } else {
+            echo '<div style="color: darkblue">';
+        }
+        echo $info.'</div>';
+    }
+    echo '<hr>';
 }
 echo '<hr>';
