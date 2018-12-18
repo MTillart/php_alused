15dbb0600f43cabec092ad09f50ac70d4e16a33a
merliti.tillart <merliti.tillart@khk.ee>
2018-12-18 12:17:54 +0200

    Array- väljastamine ja elementide töötulus

--- arrays/index.php
+++ arrays/index.php
@@ -6,8 +6,17 @@ $kasutajad[]= 'Mice';
 array_push($kasutajad,'lucy');
 
 $lisaKasutajad = array('Mup', 'Geene'); //andmetega massiiv
-echo '<pre>';
-print_r($kasutajad);
-print_r($lisaKasutajad);
-echo '</pre>';
-//var_dump($kasutajad);
+
+$kasutajadKokku = count($kasutajad);
+for($i=0;$i< $kasutajadKokku; $i++){
+    echo $kasutajad[$i].'<br>';
+}
+
+array_shift($kasutajad);
+for($i=0; $i < $kasutajadKokku; $i++){
+    echo $kasutajad[$i].'<br>';
+}
+$uuedKasutajad = array_merge($kasutajad, $lisaKasutajad);
+for($i=0; $i< $kasutajadKokku; $i++){
+    echo $uuedKasutajad[$i].'<br>';
+}
