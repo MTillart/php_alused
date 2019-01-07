00e6ef544948ae4d9a4acdcd4ddb385ed219ba2d
MTillart <merliti.tillart@khk.ee>
2019-01-07 18:52:51 +0200

    Ruumalade arvutamine

--- form/tellimine.php
+++ form/tellimine.php
@@ -8,8 +8,9 @@ $raadius = $_GET['raadius'];
 $korgus = $_GET['korgus'];
 
 $kera_ruumala = round(4/3*3.14*pow($raadius,3),1);
-$pindala_pohi =
-
+$pindala_pohi = round(3.14*pow($raadius, 2),1);
+$koonuse_ruumala = round(1/3*$pindala_pohi*$korgus,1);
+$silindri_ruumala = round($pindala_pohi*$korgus,1);
 
 
 echo 'Loom: '.$loom.'<br>';
@@ -22,16 +23,16 @@ echo '<h2>Kera ruumala</h2>';
 echo 'ruumala: '.$kera_ruumala.'cm<sup>3</sup><br>';
 echo '<br>';
 
-
-
-
 echo '<h2>Koonuse ruumala</h2>';
+echo 'Põhja pindala: '.$pindala_pohi.'cm<sup>2</sup><br>';
+echo 'Koonuse ruumala: '.$koonuse_ruumala.'cm<sup>3</sup><br>';
+
 echo '<br>';
 
-
-
-
 echo '<h2>Silindri ruumala</h2>';
+echo 'Silindri ruumala: '.$silindri_ruumala.'cm<sup>3</sup><br>';
+
+
 echo '<br>';
 
 
