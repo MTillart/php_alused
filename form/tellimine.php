ae4841631b7e9c36a5418cf79071c1d6c6cfee6f
MTillart <merliti.tillart@khk.ee>
2019-01-07 18:42:15 +0200

    Kera ruumala on arvutatud

--- form/tellimine.php
+++ form/tellimine.php
@@ -4,8 +4,34 @@ echo '<h1>Tellimine</h1>';
 $loom = $_GET['loom'];
 $lind = $_GET['lind'];
 $kala = $_GET['kala'];
+$raadius = $_GET['raadius'];
+$korgus = $_GET['korgus'];
+
+$kera_ruumala = round(4/3*3.14*pow($raadius,3),1);
+$pindala_pohi =
+
 
 
 echo 'Loom: '.$loom.'<br>';
 echo 'Lind: '.$lind.'<br>';
 echo 'Kala: '.$kala.'<br>';
+echo 'raadius: '.$raadius.'cm'.'<br>';
+echo 'korgus: '.$korgus.'cm'.'<br>';
+echo '<hr>';
+echo '<h2>Kera ruumala</h2>';
+echo 'ruumala: '.$kera_ruumala.'cm<sup>3</sup><br>';
+echo '<br>';
+
+
+
+
+echo '<h2>Koonuse ruumala</h2>';
+echo '<br>';
+
+
+
+
+echo '<h2>Silindri ruumala</h2>';
+echo '<br>';
+
+
