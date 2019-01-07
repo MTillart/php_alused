cdd13e4dbffa943b0d4b23a25ebd9a102daf860e
MTillart <merliti.tillart@khk.ee>
2019-01-07 18:02:10 +0200

    Andmed kuvatakse PHP failis

--- form/tellimine.php
+++ form/tellimine.php
@@ -1,2 +1,11 @@
 <?php
 echo '<h1>Tellimine</h1>';
+//lisab vormist saadud andmed muutujasse
+$loom = $_GET['loom'];
+$lind = $_GET['lind'];
+$kala = $_GET['kala'];
+
+
+echo 'Loom: '.$loom.'<br>';
+echo 'Lind: '.$lind.'<br>';
+echo 'Kala: '.$kala.'<br>';
