a4fa70e2654a46db631f07c6d2d7c6e45f516da1
MTillart <merliti.tillart@khk.ee>
2019-01-13 09:18:57 +0200

    Teksti vormindamine

---
+++ text/index.php
@@ -0,0 +1,11 @@
+<?php
+$tekst = "Maybe life should be about more than just surviving";
+echo strtolower($tekst);
+echo '<br>';
+echo strtoupper($tekst);
+echo '<br>';
+echo ucfirst($tekst);
+echo '<br>';
+echo ucwords($tekst);
+echo '<br>';
+//Teksti pikkus
