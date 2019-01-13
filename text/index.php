5059967f00ac677051f568d6c0f0f5625375023d
MTillart <merliti.tillart@khk.ee>
2019-01-13 13:22:11 +0200

    ülesanne 9 valmis

--- text/index.php
+++ text/index.php
@@ -62,4 +62,42 @@ $tekst5 = "Musta lehma saba musta lehma taga, valge lehma saba valge lehma taga"
 $otsi = array('lehma', 'saba', 'taga');
 $asenda = array('koera', 'nina', 'ees');
 echo str_replace($otsi, $asenda, $tekst5);
+//Ülesanne 9
+echo '<br><h3>Ülesanne 9</h3>';
+$nimi = "meRliTi";
+$nimi_fix = ucfirst(strtolower($nimi));
+echo 'Tere '.$nimi_fix;
+echo '<br>';
+//$sisend = 'stalker';
+//$tahe_algus = 0;
+//$tahe_arv = strlen($sisend);
+//echo $tahe_arv;
+//$mitmes = 0;
+//while ($mitmes = $tahe_arv){
+//    echo $sisend[$mitmes];
+//    $mitmes= $mitmes + 1;
+//}
+echo '<br>';
+$lause = "Kurat, pole parata, inimesed on ikka nõmedad";
+$eemalda = array('Kurat', 'nõmedad');
+$paranda = array('***', '***');
+echo str_replace($eemalda, $paranda, $lause);
+echo '<br>';
+//email
+$nimi_pere = "Jänes Kõpsleja";
+$alg_tahed = array('ä', 'ö', 'ü', 'õ');
+$muut_tahed= array('a', 'o', 'y', 'o');
+$nimi_pere_ok = strtolower(str_replace($alg_tahed, $muut_tahed, $nimi_pere));
+echo "Nimi, ilma keelatud tähtedeta: ".$nimi_pere_ok;
+echo '<br>';
 
+$nimed = str_word_count($nimi_pere_ok, 1);
+$eesnimi = $nimed[0];
+$perenimi = $nimed[1];
+echo 'eesnimi: '.$eesnimi;
+echo '<br>';
+echo 'perenimi: '.$perenimi;
+//emaili loomine
+echo '<br>';
+$email = $eesnimi.'.'.$perenimi.'@khk.ee';
+echo 'Email: '.$email;
