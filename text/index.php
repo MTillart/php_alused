6b73c48767aea016ad3731bd455875ea13303c06
MTillart <merliti.tillart@khk.ee>
2019-01-13 09:41:56 +0200

    Teksti pikkus ja kärpimine

--- text/index.php
+++ text/index.php
@@ -9,3 +9,23 @@ echo '<br>';
 echo ucwords($tekst);
 echo '<br>';
 //Teksti pikkus
+echo strlen($tekst);
+echo '<br>';
+echo str_word_count($tekst);
+echo '<br>';
+$tekst2 = '         Victory stands on the back of a sacrifice.      ';
+echo "<pre>$tekst2</pre>";
+echo '<br>';
+echo '<pre>'.trim($tekst2)."</pre>";
+echo '<br>';
+echo '<pre>'.ltrim($tekst2)."</pre>";
+echo '<br>';
+echo '<pre>'.rtrim($tekst2)."</pre>";
+echo '<br>';
+$tekst3 = "In the end, we are what we are!";
+echo trim($tekst3, "I, n, !");
+echo '<br>';
+$tekst4 = "In the end, <b>We are what <a href='#'>we</a> </b><i>are</i>";
+echo strip_tags($tekst4, '<b>, <i>');
+
+
