c16ef2c57ee36d997e0cf0091a4c101a5f265300
MTillart <merliti.tillart@khk.ee>
2019-01-20 18:45:29 +0200

    Failid on valmis

--- recode/index.php
+++ recode/index.php
@@ -1,44 +1,25 @@
 <?php
+?>
+<menu>
+    <a href="index.php">Siin me oleme</a>
+    <a href="kompass.php">Kompass</a>
+    <a href="raamat.php">Raamat</a>
+    <a href="oksaraas.php">Oksaraas</a>
+</menu>
+<?php
+if(!empty($_GET['leht'])){
+    $leht = htmlspecialchars($_GET['leht']);
+    $lubatud = array('kompass', 'raamat', 'oksaraas');
+    $kontroll = in_array($leht, $lubatud);
+    if($kontroll==true){
+        include($leht.'.php');
+    }
+} else{
+    echo 'Valitud leht ei eksisteeri';
+}
+?>
 
-$nimed = array(1=>'Adord', 'Catra', 'Climmer', 'Bow', 'Etheria');
-for($nr = 0; $nr<count($nimed);$nr++){
-echo $nimed[$nr]."<br>";
-}
-echo "<br>";
-//tsükkel tükklis
-for ($rida=1; $rida<=5; $rida++){
-    for($veerg=5; $veerg>=$rida; $veerg--){
-        echo "*";
-    }
-    echo "<hr>";
-}
-echo "<br>";
-for($arv=1; $arv<=10; $arv++){
-    if($arv<=3){
-        break;
-    }
-    echo $arv."<br>";
-}
-echo "<br>";
-$kokku = count($nimed);
-
-for($jk=1;$jk<=$kokku;$jk++){
-    if($jk<$kokku){
-        continue;
-    }
-    echo $nimed[$nr]."<br>";
-}
-//Ülesanne 6
-for($kk=1;$kk<=5; $kk++){
-    for($jj=1; $jj<=$kk*2;$jj++){
-        echo "--";
-    }
-    echo" *<br>";
-}
-echo "<br>";
-for($kk=1;$kk<=5; $kk++){
-    for($jj=1; $jj<=$kk;$jj++){
-        echo "$jj";
-    }
-    echo "<br>";
-}
+<h2> Siin me oleme</h2>
+<p>
+    Kenal suvepäeval veereb ühe Muhumaa rahuliku talu õuele väike sapakas, kust ilmub välja kummaline seltskond: võimukas proua, tema käpa all olev kiitsakas abikaasa ning salapärane punapäine kaunitar. Nad teatavad, et tulid tallu puhkust veetma.
+    Rahulikust puhkusest kujuneb nii pealinnast tulnud suvitajate kui neid võõrustavate muhulaste jaoks meeletu sündmuste virvarr. Kapriise ja kuraasi täis Kohviveski keerab taluelu pea peale. Nappidesse suveöödesse mahuvad veel kosjad, vastastikune rammukatsumine, angerjad lakas ja palju muud.</p>
