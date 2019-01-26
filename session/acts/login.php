07757037706661fdea7be3688c3eedad1d91d425
MTillart <merliti.tillart@khk.ee>
2019-01-26 15:59:35 +0200

    loodud kasutaja andmete kontroll andmebaasist

--- session/acts/login.php
+++ session/acts/login.php
@@ -1,1 +1,15 @@
 <?php
+
+require_once '../db/conf.php';
+require_once '../db/db_fnk.php';
+//andmed HTML vormi poolt
+$username = $_GET['uname'];
+$password = $_GET['upass'];
+
+// loome ühenduse andmebaasiga
+$ikt = connect_db(DBHOST, DBUSER, DBPASS, DBNAME);
+//küsime kasutaja andmete olemasolu andmebaasis
+$sql = 'SELECT * FROM user WHERE username = "'.$username.'" AND password= "'.md5($password).'"';
+$users = getData($sql, $ikt);
+// väljastame andmed
+echo $users[0]['first_name'].' '.$users[0]['last_name'];
